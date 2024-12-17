89d8b049a96bd9695c6c7a3823bb419a4bade739
HIDE_MAIN <ra___be@naver.com>
2024-12-17 15:52:34 +0900

    Create 4375.java

---
+++ acmicpc/4375.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    private static int solve(int n) {
+        int lastMod = 0;
+        for (int i = 1; ; i++) {
+            if ((lastMod = (lastMod * 10 + 1) % n) == 0) {
+                return i;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            System.out.println(solve(n));
+        }
+
+        sc.close();
+    }
+}
